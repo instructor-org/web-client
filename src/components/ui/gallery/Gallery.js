e51f0ad9f27dd16c35722cb17d717005c952488d
Jay <julesmontojo1@gmail.com>
2022-10-15 14:09:53 +0900

    feat: implement initial gallery stylings

---
+++ src/components/ui/gallery/Gallery.js
@@ -0,0 +1,21 @@
+import './gallery.css';
+import Card from '../card/Card';
+
+export default function Gallery() {
+  return (
+    <div className='gallery-container'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+};
