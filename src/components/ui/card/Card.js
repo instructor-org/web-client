411c6c1010311565a8d009049e246a59a8ff3a53
Jay <julesmontojo1@gmail.com>
2022-10-15 14:07:52 +0900

    feat: add initial click functionality.

--- src/components/ui/card/Card.js
+++ src/components/ui/card/Card.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './card.css';
 
 export default function Card() {
+
+  const handleCardSelect = () => {
+    console.log("I have been selected!")
+  };
+  
   return (
-    <div className='card-container'>
+    <div className='card-container' onClick={handleCardSelect}>
       <img className="card-img" src={require("./food.jpeg")} alt="food" />
-      <h2 className="card-title">Japanese</h2>
+      <h2 className="card-title">Cuisine</h2>
     </div>
   )
 }
