6937ef66a558b8b622f8d8442bd9c3eb28e40fc9
Jay <julesmontojo1@gmail.com>
2022-10-15 16:05:40 +0900

    chore: cleanup bad references

---
+++ src/pages/Categories.js
@@ -0,0 +1,10 @@
+import './categories.css';
+import '../components/ui/card/Card';
+import Gallery from '../components/ui/gallery/Gallery';
+
+export default function Categories() {
+  return (
+    <>
+    </>
+  )
+}
