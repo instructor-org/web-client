fcd7fc48ff97d2894cdf9d2aa48c48d3de78e657
Jay <julesmontojo1@gmail.com>
2022-10-21 22:32:41 +0900

    feat: initial mock-up html and css

--- src/pages/establishment/Establishment.js
+++ src/pages/establishment/Establishment.js
@@ -7,6 +7,16 @@ export default function Establishment() {
   return (
     <>
       <Banner />
+      <div className="header">
+        <h1 className="establishment__name">SushiDon</h1>
+        <div className='icon__container'></div>
+      </div>
+      <div className="review__carousel">
+        <div className="review__card"></div>
+        <div className="review__card"></div>
+        <div className="review__card"></div>
+        <div className="review__card"></div>
+      </div>
     </>
   )
 }
